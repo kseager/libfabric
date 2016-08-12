14374aef2482443317ba0cbed0ce0fb9d197ee2a
kseager <kayla.seager@intel.com>
2016-08-11 18:25:31 -0700

    check retrieve_verify restart build

--- prov/util/src/util_mr.c
+++ prov/util/src/util_mr.c
@@ -124,25 +124,34 @@ int ofi_mr_insert(struct ofi_util_mr * in_mr_h, const struct fi_mr_attr *in_attr
     return 0;
 }
 
-static void * mr_retrieve_item(struct ofi_util_mr * in_mr_h,  uint64_t in_key)
+static struct fi_mr_attr *  mr_retrieve_item(struct ofi_util_mr * in_mr_h,  uint64_t in_key)
 {
     void * itr;
-    struct fi_mr_attr * item;
+    void * item;
+    void *mr_key = &in_key;
 
-    itr = rbtFind(in_mr_h->map_handle, &in_key);
+    itr = rbtFind(in_mr_h->map_handle, mr_key);
 
     if (!itr)
         return NULL;
 
-    rbtKeyValue(in_mr_h->map_handle, itr, (void **)&in_key,
-                                (void **) &item);
-
-    return item;
+    rbtKeyValue(in_mr_h->map_handle, itr, &mr_key, &item);
+    return (struct fi_mr_attr *) item;
 }
 
 void * ofi_mr_retrieve(struct ofi_util_mr * in_mr_h,  uint64_t in_key)
 {
-    struct fi_mr_attr * item = mr_retrieve_item(in_mr_h, in_key);
+    void * itr;
+    struct fi_mr_attr * item;
+    void * key = &in_key;
+    itr = rbtFind(in_mr_h->map_handle, key);
+
+    if(!itr)
+        return NULL;
+
+    rbtKeyValue(in_mr_h->map_handle, itr, &key, (void **) &item);
+
+    //struct fi_mr_attr * item =  mr_retrieve_item(in_mr_h, in_key);
     return item->context;
 }
 
@@ -151,22 +160,61 @@ int ofi_mr_retrieve_and_verify(struct ofi_util_mr * in_mr_h, ssize_t in_len,
                                 uintptr_t *io_addr, uint64_t in_key,
                                 uint64_t in_access, void **out_prov_mr)
 {
-    int ret = 0;
-    struct fi_mr_attr * item = mr_retrieve_item(in_mr_h, in_key);
-    /*return providers MR struct */
-    if (!item || !io_addr)
+    void * itr;
+    //struct fi_mr_attr * item =  mr_retrieve_item(in_mr_h, in_key);
+    int err = 0;
+
+    assert(io_addr);
+
+    struct fi_mr_attr * item;// = mr_retrieve_item(in_mr_h, in_key);
+    void * key = &in_key;
+    itr = rbtFind(in_mr_h->map_handle, key);
+
+    if(!itr)
+        return -FI_ENOKEY;
+
+    rbtKeyValue(in_mr_h->map_handle, itr, &key, (void **) &item);
+
+
+    //return providers MR struct
+    if(!item)
+        return -FI_EINVAL;
+
+    (*out_prov_mr) = item->context;
+
+    //offset for scalable
+    if(in_mr_h->mr_type == FI_MR_SCALABLE)
+        *io_addr = (*io_addr) + item->offset;
+
+    err = verify_addr(in_mr_h, item, in_access, *io_addr, in_len);
+    if(err)
+       return err;
+
+    return 0;
+    /*int ret = 0;
+    void * itr;
+    struct fi_mr_attr * item;// = mr_retrieve_item(in_mr_h, in_key);
+    itr = rbtFind(in_mr_h->map_handle, &in_key);
+
+    if(!itr)
+        return -FI_ENOKEY;
+
+    rbtKeyValue(in_mr_h->map_handle, itr, (void **)&in_key,
+                                (void **) &item);
+
+    if (!item)
         return -FI_EINVAL;
 
     if (out_prov_mr)
         (*out_prov_mr) = item->context;
 
-    /*offset for scalable */
+    //offset for scalable
     if (in_mr_h->mr_type == FI_MR_SCALABLE)
         *io_addr = (*io_addr) + item->offset;
 
     ret = verify_addr(in_mr_h, item, in_access, *io_addr, in_len);
 
-    return ret;
+    return ret;*/
 }
 
 int ofi_mr_erase(struct ofi_util_mr * in_mr_h, uint64_t in_key)
